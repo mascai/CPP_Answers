dbfc24a84a107ecea6e5646d25769a0993b0b4f3
Alex <msumoskalenko@gmail.com>
2021-01-24 19:20:16 +0300

    Create 01_static_cast.cpp

---
+++ 06_type_cast/01_static_cast.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// What is the static_cast
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_data(void* data) {
+    cout << data << endl;
+}
+
+int main(){
+    char* ptr = new char[10];
+    ptr = "abc";
+    void* ptr2 = ptr;
+    print_data(ptr2);
+}
+
